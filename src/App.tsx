1760360fcd8cfaf6f54d3d297b14a5b1b0bbefb7
ilfey <ilfey.global@gmail.com>
2023-07-06 23:22:44 +0700

    add load more inverse

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,76 @@
 import { useState } from 'react'
-import { useGetMessagesQuery } from './api'
+import { IMessagesRequest, api, useGetMessagesQuery } from './api'
 import Message from './components/Message'
+import { useDispatch } from 'react-redux'
+
+const
+    LOAD_MORE = 'load-more',
+    LOAD_MORE_INVERSED = 'load-more-inversed',
+    INFINITY = 'infinity'
 
 
 export function App() {
-    const [cursor, setCursor] = useState(0)
-    const [isInfinity, setIsInfinity] = useState(false)
+    const [layoutShiftType, setLayoutShiftType] = useState(LOAD_MORE)
 
-    const { data, isLoading, isError } = useGetMessagesQuery(cursor)
+    const dispatch = useDispatch()
+
+    const [args, setArgs] = useState<IMessagesRequest>({
+        cursor: 0,
+        limit: 10,
+        inverse: false,
+    })
+
+    const { data, isLoading, isError } = useGetMessagesQuery(args)
 
     return (
         <>
             {/* header for mobile */}
             <header className='border-b border-gray-500'>
-                    <p className='text-center'>
-                        cursor: <b>{cursor}</b> <br />
-                    </p>
+                <p className='text-center'>
+                    cursor: <b>{args.cursor}</b> <br />
+                </p>
 
-                    <p className='text-center'>
-                        item count: <b>{data?.length ?? 0}</b>
-                    </p>
-                
-                <select className='block mx-auto bg-transparent' value={isInfinity ? 'true' : 'false'} onChange={e => setIsInfinity(e.currentTarget.value === "true")}>
-                    <option value="false" className='bg-gray-700'>Load more</option>
-                    <option value="true" className='bg-gray-700'>Infinity</option>
+                <p className='text-center'>
+                    item count: <b>{data?.length ?? 0}</b>
+                </p>
+
+                <select className='block mx-auto bg-transparent' value={layoutShiftType} onChange={e => {
+                    const nextLayoutShiftType = e.currentTarget.value  
+                    setLayoutShiftType(nextLayoutShiftType)
+                    dispatch(api.util.resetApiState()) // for clear cache
+                    if (nextLayoutShiftType === LOAD_MORE_INVERSED) {
+                        setArgs({
+                            ...args,
+                            cursor: 0,
+                            inverse: true
+                        })
+                    } else {
+                        setArgs({
+                            ...args,
+                            cursor: 0,
+                            inverse: false
+                        })
+                    }
+                    }}>
+                    <option value={LOAD_MORE} className='bg-gray-700'>Load more</option>
+                    <option value={LOAD_MORE_INVERSED} className='bg-gray-700'>Load more inversed</option>
+                    <option value={INFINITY} className='bg-gray-700'>Infinity</option>
                 </select>
             </header>
 
             <main className='flex flex-col items-center'>
-                {!isInfinity && (
+                {layoutShiftType === LOAD_MORE && (
                     <button className='rounded-md border border-gray-700 p-2  mt-3'
-                        onClick={() => setCursor(data === undefined ? 0 : data[0].id)}>
+                        onClick={() => setArgs({
+                            ...args,
+                            ...{ cursor: data === undefined ? 0 : data[0].id }
+                        })}>
                         {isError ? 'Retry...' : 'Load more...'}
                     </button>
                 )}
                 <div className='px-6 max-w-3xl w-full flex flex-col gap-4 mt-3'>
                     {isLoading ?
-                        "Loading..." :
+                        <p className='text-center'>Loading...</p> :
                         data?.map((msg, index) =>
                             <Message
                                 index={index}
@@ -45,6 +79,18 @@ export function App() {
                         )
                     }
                 </div>
+                {layoutShiftType === LOAD_MORE_INVERSED && (
+                    <button className='rounded-md border border-gray-700 p-2  mt-3'
+                        onClick={() => setArgs({
+                            ...args,
+                            ...{ 
+                                cursor: data === undefined ? 0 : data[data.length-1].id,
+                                inverse: true
+                             }
+                        })}>
+                        {isError ? 'Retry...' : 'Load more...'}
+                    </button>
+                )}
             </main>
         </>
     )
--- src/api.ts
+++ src/api.ts
@@ -1,15 +1,25 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { IMessage } from "./etities/Message";
 
+export interface IMessagesRequest {
+    cursor: number
+    limit: number
+    inverse: boolean
+}
+
 export const api = createApi({
     reducerPath: "",
     baseQuery: fetchBaseQuery({ baseUrl: "https://devback-pr-2.onrender.com/api/v1/" }),
     endpoints: (builder) => ({
-        getMessages: builder.query<Array<IMessage>, number>({
-            query: (cursor) => `messages?cursor=${cursor}&limit=10`,
+        getMessages: builder.query<Array<IMessage>, IMessagesRequest>({
+            query: ({ cursor, limit, inverse }) => `messages?cursor=${cursor}&limit=${inverse ? limit + '&inverse' : limit}`,
             serializeQueryArgs: ({ endpointName }) => endpointName,
             merge: (currentCache, newItems) => {
-                currentCache.unshift(...newItems);
+                if (currentCache[0].id > newItems[0].id) {
+                    currentCache.unshift(...newItems);
+                } else if (currentCache[0].id < newItems[0].id) {
+                    currentCache.push(...newItems);
+                } 
             },
             forceRefetch({ currentArg, previousArg }) {
                 return currentArg !== previousArg;
